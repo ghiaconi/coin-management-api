e5bc3595bf1d16b8cab1e0ab88e5b71a4afc1f74
ghiaconi <iaconimgheorghe@gmail.com>
2023-12-02 12:02:22 +0100

    Use markets endpoint to get tokens data - only one call (250 items/page) - has all data that is currently needed - no further data processing(filtering) necessary

--- app/services/token_service.py
+++ app/services/token_service.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from ..models.token import db, Token
 from ..utils.exceptions import *
 
@@ -7,6 +7,10 @@ from ..utils.exceptions import *
 class TokenService:
     coingecko_base_url = 'https://api.coingecko.com/api/v3'
     coin_endpoint = f'{coingecko_base_url}/coins/'
+    markets_endpoint = f'{coingecko_base_url}/coins/markets'
+    calls_per_minute_limit = 30
+    data_refreshed_every = 60  # seconds
+    items_per_page_limit = 250
 
     def get_or_create_token(self, token_key):
         existing_token = Token.query.filter_by(key=token_key).first()
@@ -27,7 +31,7 @@ class TokenService:
 
     def create_token(self, token_key):
         try:
-            token_info = self.fetch_token_data(token_key)
+            token_info = self.fetch_tokens_market_data(token_key)[0]
 
             new_token = Token(
                 key=token_info.get('id', token_key),
@@ -42,54 +46,50 @@ class TokenService:
         except TokenNotFoundError as e:
             raise e
 
-    def fetch_token_data(self, token_key):
+    # TODO split in 2 or use the dedicated /coin endpoint when searching for one token
+    def fetch_tokens_market_data(self, ids, page=1):
+        ids_to_query = ','.join([ids] if not isinstance(ids, list) else ids)
+
         params = {
-            'localization': 'false',
-            'community_data': 'false',
-            'developer_data': 'false',
+            'ids': ids_to_query,
+            'vs_currency': 'eur',
+            'order': 'market_cap_desc',
+            'per_page': self.items_per_page_limit,
+            'page': page,
             'sparkline': 'true',
         }
 
         try:
-            response = requests.get(self.coin_endpoint + token_key, params=params)
+            response = requests.get(self.markets_endpoint, params=params)
             response.raise_for_status()
+            tokens_data = response.json()
 
-            token_info = response.json()
+            if len(tokens_data) == 0:
+                raise TokenNotFoundError(f'Token not found: {ids}')
 
-            if token_info:
-                return token_info
-            else:
-                raise TokenNotFoundError(f'Token not found: {token_key}')
+            return response.json()
+
         except requests.HTTPError as e:
-            if e.response.status_code == 404:
-                raise TokenNotFoundError(f'Token not found: {token_key}')
-            else:
-                raise TokenServiceError(f'Request error occurred: {str(e)}')
+            raise TokenServiceError(f'Request error occurred: {str(e)}')
+
         except requests.Timeout:
             raise TokenServiceError('Timeout error occurred.')
+
         except requests.RequestException as e:
             raise TokenServiceError(f'Request error occurred: {str(e)}')
 
-    # def populate_table_with_popular_tokens(self):
-    #     popular_tokens = ['bitcoin', 'ethereum', 'ripple', 'litecoin', 'cardano']
-    #     results = []
-    #
-    #     for token_key in popular_tokens:
-    #         res, data = self.create_token(token_key)
-    #         results.append((res, data))
-    #
-    #     return results
-
-    # def update_existing_tokens(self):  # TODO: add to a cron job
+    # def refresh_tokens(self):  # TODO: add to a cron job
     #     try:
     #         existing_tokens = Token.query.all()
     #
     #         for token in existing_tokens:
-    #             last_update_time = token.last_update_time or datetime(1970, 1, 1)
-    #             time_since_last_update = datetime.utcnow() - last_update_time
+    #             last_update_str = token.attributes['last_updated'] or datetime(1970, 1, 1)
+    #             last_update = datetime.strptime(last_update_str, '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=timezone.utc)
+    #             current_time = datetime.now(timezone.utc)
+    #             time_since_last_update = current_time - last_update
     #
     #             if time_since_last_update > timedelta(hours=24):
-    #                 token_info = self.get_token_data(token.key)
+    #                 token_info = self.fetch_tokens_market_data(token.key)
     #
     #                 if token_info:
     #                     token.attributes = token_info['attributes']
@@ -98,4 +98,14 @@ class TokenService:
     #         db.session.commit()
     #     except Exception as e:
     #         print(f"Error updating tokens: {str(e)}")
-
+    #
+    # def update_expired_tokens(self):
+    #     # Sort tokens based on the oldest last_update_time
+    #     sorted_tokens = sorted(token_data.items(), key=lambda x: x[1]['last_update_time'])
+    #
+    #     # Batch tokens based on the rate limit (30 calls/minute)
+    #     batch_size = items_per_page_limit
+    #     for i in range(0, len(sorted_tokens), batch_size):
+    #         batch_tokens = dict(sorted_tokens[i:i + batch_size])
+    #         for token in batch_tokens:
+    #             fetch_data_for_token(token)
